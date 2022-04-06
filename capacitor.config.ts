c7fb6dd3842a43652de03624cc03e615c3e2567c
Vidal Lindão <vidal.cabrera@edu.unifil.br>
2022-04-06 18:57:04 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calculadora2022pippo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
